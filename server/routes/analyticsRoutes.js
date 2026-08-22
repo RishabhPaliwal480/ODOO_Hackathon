1d0a9e6d879cbe140a52ebde32c0905a479bd60c
Rishi Sharma <rishisharma3101@gmail.com>
2026-08-22 17:55:18 +0530

    Delete server directory

--- server/routes/analyticsRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getGlobalAnalytics } = require('../controllers/analyticsController');
-
-router.get('/', getGlobalAnalytics);
-
-module.exports = router;
--- server/routes/cityRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCities, getCityDetails } = require('../controllers/cityController');
-
-router.get('/', getCities);
-router.get('/:id', getCityDetails);
-
-module.exports = router;
